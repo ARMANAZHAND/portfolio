45d8013e1e0c17b24f7d96a2194273c970701241
Arman Azhand <arman.azhand@gmail.com>
2026-04-14 15:04:24 -0700

    Update projects.js
    replaced with temporary projects while capstone is being worked on

--- js/projects.js
+++ js/projects.js
@@ -1,23 +1,23 @@
 export const projects = [
     {
-        title: "Project 1 TEMPORARY",
-        description: "FILL THIS OUT",
-        tags: ["React", "Node.js", "MongoDB", "Stripe"],
-        liveLink: "#",
-        repoLink: "https://github.com/armanazhand"
+        title: "Financial Loss Minimization",
+        description: "A case study on financial loss minimization using various statistical and machine learning techniques.",
+        tags: ["Python", "Scikit-learn", "Matplotlib", "NumPy", "Pandas", "XGBoost", "Grid Search", "Random Forest", "Logistic Regression"],
+        liveLink: "https://github.com/ARMANAZHAND/Financial-Loss-Mitigation-Study---AA",
+        repoLink: "https://github.com/ARMANAZHAND/Financial-Loss-Mitigation-Study---AA"
     },
     {
-        title: "Project 2 TEMPORARY",
-        description: "FILL THIS OUT",
-        tags: ["HTML5", "CSS3", "JavaScript"],
-        liveLink: "https://armanazhand.github.io/portfolio",
-        repoLink: "https://github.com/armanazhand/portfolio"
+        title: "London Weather Time Series Analysis",
+        description: "Analysis of London weather data using time series analysis techniques.",
+        tags: ["R", "Time Series", "Data Analysis"],
+        liveLink: "https://github.com/ARMANAZHAND/London-Weather-Time-Series-Analysis/",
+        repoLink: "https://github.com/ARMANAZHAND/London-Weather-Time-Series-Analysis/"
     },
     {
-        title: "Project 3 TEMPORARY",
-        description: "FILL THIS OUT",
-        tags: ["Vue.js", "Firebase", "CSS3"],
-        liveLink: "#",
-        repoLink: "https://github.com/armanazhand"
+        title: "Contrastive Learning Presentation",
+        description: "In-depth presentation on contrastive learning, the mathematics behind it, innovations through time, and its applications in machine learning.",
+        tags: ["Contrastive Learning", "AI Prompting"],
+        liveLink: "https://contrastive-learning.static2.website/",
+        repoLink: "https://github.com/ARMANAZHAND/Contrastive-Learning---Artificial-Intelligence-2-Project"
     }
 ];
